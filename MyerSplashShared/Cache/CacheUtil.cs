448c2969c2ec4aed8320054f2a76aab3b726455d
juniperphoton <dengweichao@hotmail.com>
2016-08-13 14:16:23 +0800

    Update CacheUtil.

--- MyerSplashShared/Cache/CacheUtil.cs
+++ MyerSplashShared/Cache/CacheUtil.cs
@@ -11,20 +11,9 @@ using Windows.Storage;
 
 namespace MyerSplashShared.API
 {
-    [Obsolete]
-    public class CacheUtil
+    public static class CacheUtil
     {
-        /// <summary>
-        /// 第一项是URL 地址，第二是文件的本地 Path
-        /// </summary>
-        private Dictionary<string, string> CachedFiles { get; set; }
-
-        public CacheUtil()
-        {
-
-        }
-
-        public async Task CleanUpAsync()
+        public static async Task CleanUpAsync()
         {
             var tempFolder = GetCachedFileFolder();
             var items = await tempFolder.GetItemsAsync();
@@ -32,59 +21,6 @@ namespace MyerSplashShared.API
             {
                 await item.DeleteAsync(StorageDeleteOption.PermanentDelete);
             }
-            CachedFiles.Clear();
-            await SaveAsync();
-        }
-
-        public async Task<StorageFile> DownloadImageAsync(string url, string desireName = "img.jpg")
-        {
-            if (CachedFiles.ContainsKey(url))
-            {
-                return await StorageFile.GetFileFromPathAsync(CachedFiles[url]);
-            }
-            using (var stream = await FileDownloadUtil.GetIRandomAccessStreamFromUrlAsync(url, CTSFactory.MakeCTS().Token))
-            {
-                var newFile = await GetTempFolder().CreateFileAsync(desireName, CreationCollisionOption.GenerateUniqueName);
-                var bytes = new byte[stream.AsStream().Length];
-                await stream.AsStream().ReadAsync(bytes, 0, (int)stream.AsStream().Length);
-                await FileIO.WriteBytesAsync(newFile, bytes);
-
-                CachedFiles.Add(url, newFile.Path);
-                await SaveAsync();
-                return newFile;
-            }
-        }
-
-        public async Task SaveAsync()
-        {
-            var tempFolder = GetCachedFileFolder();
-            await SerializerHelper.SerializerToJson<Dictionary<string, string>>(CachedFiles, "CachedFiles", tempFolder, true);
-        }
-
-        public async Task LoadAsync()
-        {
-            try
-            {
-                var tempFolder = GetCachedFileFolder();
-                this.CachedFiles = await SerializerHelper.DeserializeFromJsonByFile<Dictionary<string, string>>("CachedFiles", tempFolder);
-                if (this.CachedFiles == null)
-                {
-                    CachedFiles = new Dictionary<string, string>();
-                }
-            }
-            catch (Exception)
-            {
-                CachedFiles = new Dictionary<string, string>();
-            }
-        }
-
-        public async Task<StorageFile> GetCachedFile(string url)
-        {
-            if (CachedFiles.ContainsKey(url))
-            {
-                return await StorageFile.GetFileFromPathAsync(CachedFiles[url]);
-            }
-            return null;
         }
 
         public static StorageFolder GetCachedFileFolder()
