64fe45cc0153afdd7cd9c62239c75c28099eff82
juniperphoton <dengweichao@hotmail.com>
2016-09-10 01:59:32 +0800

    Update ImageEx.

--- CustomControlNative/pch.h
+++ CustomControlNative/pch.h
@@ -2,3 +2,8 @@
 
 #include <collection.h>
 #include <ppltasks.h>
+
+#define typeof(x) Windows::UI::Xaml::Interop::TypeName(x::typeid)
+#define nameof(x) #x
+
+#include "ImageEx.h"
---
+++ TestProj/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace TestProj
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var folder = await Package.Current.InstalledLocation.GetFolderAsync("Assets");
+            var file = await folder.GetFileAsync("test0.jpg");
+            IEX.FilePath = file.Path;
+            IEX.Invalidate();
+        }
+    }
+}
