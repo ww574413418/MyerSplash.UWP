1ad2558cf502141ee5ce080e32441c9fe3ae586f
juniperphoton <dengweichao@hotmail.com>
2017-06-17 12:44:24 +0800

    Revert "Temp: refactor navigation"
    This reverts commit 3f6721681c3a597dc35631ef33cc2280f76a364a.

--- MyerSplash/View/MainPage.xaml.cs
+++ MyerSplash/View/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using CompositionHelper;
-using MyerSplash.Common;
+﻿using MyerSplash.Common;
 using MyerSplash.Model;
 using MyerSplash.ViewModel;
 using MyerSplashShared.Utils;
@@ -9,6 +8,7 @@ using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
@@ -22,6 +22,8 @@ namespace MyerSplash.View
         private MainViewModel MainVM { get; set; }
 
         private Compositor _compositor;
+        private Visual _drawerVisual;
+        private Visual _drawerMaskVisual;
         private Visual _titleGridVisual;
         private Visual _refreshBtnVisual;
         private Visual _titleStackVisual;
@@ -53,12 +55,38 @@ namespace MyerSplash.View
             else page.ShowLoading();
         }
 
+        public bool DrawerOpended
+        {
+            get { return (bool)GetValue(DrawerOpendedProperty); }
+            set { SetValue(DrawerOpendedProperty, value); }
+        }
+
+        public static readonly DependencyProperty DrawerOpendedProperty =
+            DependencyProperty.Register("DrawerOpended", typeof(bool), typeof(MainPage),
+                new PropertyMetadata(false, OnDrawerOpenedPropertyChanged));
+
+        public static void OnDrawerOpenedPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var page = d as MainPage;
+            page.ToggleDrawerAnimation((bool)e.NewValue);
+            page.ToggleDrawerMaskAnimation((bool)e.NewValue);
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
             this.DataContext = MainVM = new MainViewModel();
+            this.Loaded += MainPage_Loaded;
             InitComposition();
             InitBinding();
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            _drawerMaskVisual.Opacity = 0;
+            _drawerVisual.Offset = new Vector3(-DRAWER_WIDTH, 0f, 0f);
+
+            DrawerMaskBorder.Visibility = Visibility.Collapsed;
         }
 
         private void InitBinding()
@@ -70,14 +98,24 @@ namespace MyerSplash.View
                 Mode = BindingMode.TwoWay,
             };
             this.SetBinding(IsLoadingProperty, b);
+
+            var b2 = new Binding()
+            {
+                Source = MainVM,
+                Path = new PropertyPath("DrawerOpened"),
+                Mode = BindingMode.TwoWay,
+            };
+            this.SetBinding(DrawerOpendedProperty, b2);
         }
 
         private void InitComposition()
         {
-            _compositor = this.GetVisual().Compositor;
-            _titleGridVisual = TitleGrid.GetVisual();
-            _refreshBtnVisual = RefreshBtn.GetVisual();
-            _titleStackVisual = NavigationView.GetVisual();
+            _compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            _drawerVisual = ElementCompositionPreview.GetElementVisual(DrawerControl);
+            _drawerMaskVisual = ElementCompositionPreview.GetElementVisual(DrawerMaskBorder);
+            _titleGridVisual = ElementCompositionPreview.GetElementVisual(TitleGrid);
+            _refreshBtnVisual = ElementCompositionPreview.GetElementVisual(RefreshBtn);
+            _titleStackVisual = ElementCompositionPreview.GetElementVisual(TitleStack);
         }
 
         #region Loading animation
@@ -91,6 +129,39 @@ namespace MyerSplash.View
             ListControl.Refreshing = false;
         }
         #endregion
+
+        #region Drawer animation
+        private void ToggleDrawerAnimation(bool show)
+        {
+            var offsetAnim = _compositor.CreateScalarKeyFrameAnimation();
+            offsetAnim.InsertKeyFrame(1f, show ? 0f : -DRAWER_WIDTH);
+            offsetAnim.Duration = TimeSpan.FromMilliseconds(300);
+
+            _drawerVisual.StartAnimation("Offset.X", offsetAnim);
+        }
+
+        private void ToggleDrawerMaskAnimation(bool show)
+        {
+            if (show) DrawerMaskBorder.Visibility = Visibility.Visible;
+
+            var fadeAnimation = _compositor.CreateScalarKeyFrameAnimation();
+            fadeAnimation.InsertKeyFrame(1f, show ? 1f : 0f);
+            fadeAnimation.Duration = TimeSpan.FromMilliseconds(500);
+
+            var batch = _compositor.CreateScopedBatch(CompositionBatchTypes.Animation);
+            _drawerMaskVisual.StartAnimation("Opacity", fadeAnimation);
+            batch.Completed += (sender, e) =>
+              {
+                  if (!show) DrawerMaskBorder.Visibility = Visibility.Collapsed;
+              };
+            batch.End();
+        }
+        #endregion
+
+        private void StackPanel_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ListControl.ScrollToTop();
+        }
 
         private async void ListControl_OnClickItemStarted(UnsplashImageBase img, FrameworkElement container)
         {
@@ -179,6 +250,83 @@ namespace MyerSplash.View
         }
         #endregion
 
+        #region Drawer manipulation
+        private void TouchGrid_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            if (e.Cumulative.Translation.X >= 70)
+            {
+                if (!DrawerOpended)
+                {
+                    DrawerOpended = true;
+                }
+                else
+                {
+                    ToggleDrawerAnimation(true);
+                    ToggleDrawerMaskAnimation(true);
+                }
+
+                NavigationService.AddOperation(() =>
+                {
+                    if (MainVM.DrawerOpened)
+                    {
+                        MainVM.DrawerOpened = false;
+                        return true;
+                    }
+                    return false;
+                });
+            }
+            else
+            {
+                if (DrawerOpended)
+                {
+                    DrawerOpended = false;
+                }
+                else
+                {
+                    ToggleDrawerAnimation(false);
+                    ToggleDrawerMaskAnimation(false);
+                }
+            }
+        }
+
+        private void TouchGrid_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            if (_drawerMaskVisual.Opacity < 1)
+            {
+                DrawerMaskBorder.Visibility = Visibility.Visible;
+                _drawerMaskVisual.Opacity += 0.02f;
+            }
+            var targetOffsetX = _drawerVisual.Offset.X + e.Delta.Translation.X;
+            _drawerVisual.Offset = new Vector3((float)(targetOffsetX > 1 ? 1 : targetOffsetX), 0f, 0f);
+        }
+
+        private void DrawerControl_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            if (e.Delta.Translation.X > 0) return;
+
+            if (_drawerMaskVisual.Opacity > 0)
+            {
+                _drawerMaskVisual.Opacity -= 0.01f;
+            }
+            var targetOffsetX = _drawerVisual.Offset.X - Math.Abs(e.Delta.Translation.X);
+            _drawerVisual.Offset = new Vector3((float)(targetOffsetX <= -300f ? -300f : targetOffsetX), 0f, 0f);
+        }
+
+        private void DrawerControl_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            //DrawerMaskBorder.Visibility = Visibility.Collapsed;
+
+            if (e.Cumulative.Translation.X < 0)
+            {
+                DrawerOpended = false;
+            }
+            else
+            {
+
+            }
+        }
+        #endregion
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
@@ -205,11 +353,6 @@ namespace MyerSplash.View
 
         private void TitleGrid_PointerExited(object sender, PointerRoutedEventArgs e)
         {
-        }
-
-        private void NavigationView_OnTapTitle(object sender, EventArgs e)
-        {
-            ListControl.ScrollToTop();
         }
     }
 }
