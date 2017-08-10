bcdc77e71a65930be610b8747c89a49eb2315410
juniperphoton <dengweichao@hotmail.com>
2017-08-11 01:15:51 +0800

    Add some tests

---
+++ UnitTestProject/Service/ImageServiceTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MyerSplash.Data;
+using MyerSplashShared.API;
+using MyerSplashShared.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject.Service
+{
+    [TestClass]
+    public class ImageServiceTest
+    {
+        private ImageServiceBase _newImageService = new ImageService(UrlHelper.GetNewImages,
+            new UnsplashImageFactory(false));
+
+        private ImageServiceBase _featuredImageService = new ImageService(UrlHelper.GetFeaturedImages,
+            new UnsplashImageFactory(true));
+
+        private ImageServiceBase _randomImageService = new RandomImageService(new UnsplashImageFactory(false));
+
+        private SearchImageService _searchImageService = new SearchImageService(new UnsplashImageFactory(false));
+
+        [TestMethod]
+        public async Task TestGetNewImages()
+        {
+            var result = await _newImageService.GetImagesAsync(CTSFactory.MakeCTS().Token);
+            Assert.IsTrue(result?.Count() > 0);
+        }
+
+        [TestMethod]
+        public async Task TestGetFeatureImages()
+        {
+            var result = await _featuredImageService.GetImagesAsync(CTSFactory.MakeCTS().Token);
+            Assert.IsTrue(result?.Count() > 0);
+        }
+
+        [TestMethod]
+        public async Task TestGetRandomImages()
+        {
+            var result = await _randomImageService.GetImagesAsync(CTSFactory.MakeCTS().Token);
+            Assert.IsTrue(result?.Count() > 0);
+        }
+
+        [TestMethod]
+        public async Task TestSearchHasResultImages()
+        {
+            _searchImageService.Query = "sea";
+            var result = await _searchImageService.GetImagesAsync(CTSFactory.MakeCTS().Token);
+            Assert.IsTrue(result?.Count() > 0);
+        }
+
+        [TestMethod]
+        public async Task TestSearchHasNoResultImages()
+        {
+            _searchImageService.Query = "dwerewrwefsdfwe";
+            var result = await _searchImageService.GetImagesAsync(CTSFactory.MakeCTS().Token);
+            Assert.IsTrue(result?.Count() == 0);
+        }
+    }
+}
